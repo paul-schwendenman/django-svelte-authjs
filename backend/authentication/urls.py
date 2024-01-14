b709247e5a7a6d807e01883dfdfb211c4b2ca161
Paul Schwendenman <schwendenman.paul@gmail.com>
2024-01-13 21:52:31 -0500

    Add token blacklist

--- backend/authentication/urls.py
+++ backend/authentication/urls.py
@@ -3,7 +3,7 @@ from dj_rest_auth.jwt_auth import get_refresh_view
 from dj_rest_auth.registration.views import RegisterView
 from dj_rest_auth.views import LoginView, LogoutView, UserDetailsView
 from django.urls import path
-from rest_framework_simplejwt.views import TokenVerifyView
+from rest_framework_simplejwt.views import TokenBlacklistView, TokenVerifyView
 
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="rest_register"),
@@ -12,4 +12,5 @@ urlpatterns = [
     path("user/", UserDetailsView.as_view(), name="rest_user_details"),
     path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
     path("token/refresh/", get_refresh_view().as_view(), name="token_refresh"),
+    path('token/blacklist/', TokenBlacklistView.as_view(), name='token_blacklist'),
 ]
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'rest_framework_simplejwt',
+    'rest_framework_simplejwt.token_blacklist',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
