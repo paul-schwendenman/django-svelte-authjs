1e9449ccc5ffcba171ff71ee59efd335352de613
Paul Schwendenman <schwendenman.paul@gmail.com>
2024-01-18 00:31:47 -0500

    Add github auth flow

---
+++ frontend/src/hooks.server.ts
@@ -0,0 +1,8 @@
+import { SvelteKitAuth } from '@auth/sveltekit';
+import GitHub from '@auth/sveltekit/providers/github';
+import { GITHUB_ID, GITHUB_SECRET, NEXTAUTH_SECRET } from '$env/static/private';
+
+export const handle = SvelteKitAuth({
+	providers: [GitHub({ clientId: GITHUB_ID, clientSecret: GITHUB_SECRET })],
+	secret: NEXTAUTH_SECRET
+});
