941caf40cd0a07a610488049a422ef83e0bcee92
Paul Schwendenman <schwendenman.paul@gmail.com>
2024-01-18 23:40:51 -0500

    Fix port on frontend

--- backend/authentication/views.py
+++ backend/authentication/views.py
@@ -6,12 +6,12 @@ from allauth.socialaccount.providers.oauth2.client import OAuth2Client
 
 class GoogleLogin(SocialLoginView):
     adapter_class = GoogleOAuth2Adapter
-    callback_url = "http://127.0.0.1:3000/"
+    callback_url = "http://127.0.0.1:5173/"
     client_class = OAuth2Client
 
 
 class GithubLogin(SocialLoginView):
     adapter_class = GitHubOAuth2Adapter
-    callback_url = "http://127.0.0.1:3000/"
+    callback_url = "http://127.0.0.1:5173/"
     client_class = OAuth2Client
 
