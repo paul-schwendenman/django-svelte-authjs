7e176213e3f380cec26decc5db68a4d5436b41d7
Paul Schwendenman <schwendenman.paul@gmail.com>
2024-01-21 17:21:00 -0500

    Add more providers to sign page
    JSX example: https://github.com/nextauthjs/next-auth/blob/main/packages/core/src/lib/pages/signin.tsx#L49-L241

--- frontend/src/routes/login/+page.svelte
+++ frontend/src/routes/login/+page.svelte
@@ -9,23 +9,30 @@
   console.log({csrfToken})
 </script>
 
-<form method="post" action="/auth/callback/credentials">
-    <input name="csrfToken" type="hidden" value={csrfToken} />
-    <label>
-      Username
-      <input name="username" type="text" />
-    </label>
-    <label>
-      Password
-      <input name="password" type="password" />
-    </label>
-    <button type="submit">Sign in</button>
-</form>
 
 {#each Object.values(providers) as provider (provider.id)}
-<div>
-  <button on:click={() => signIn(provider.id)}>
-    Sign in with {provider.name}
-  </button>
-</div>
+  {#if provider.type == 'oauth'}
+    <div>
+      <button on:click={() => signIn(provider.id)}>
+        Sign in with {provider.name}
+      </button>
+    </div>
+  {:else if provider.type == 'credentials'}
+    <form method="post" action={provider.callbackUrl}>
+        <input name="csrfToken" type="hidden" value={csrfToken} />
+        <label>
+          Username
+          <input name="username" type="text" />
+        </label>
+        <label>
+          Password
+          <input name="password" type="password" />
+        </label>
+        <button type="submit">Sign in</button>
+    </form>
+  {:else}
+  <pre>
+    {JSON.stringify(provider, null, 4)}
+  </pre>
+  {/if}
 {/each}
