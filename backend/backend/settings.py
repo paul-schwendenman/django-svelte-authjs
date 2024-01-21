2c8667f9fb0b65577ff1cb664a9478baf5953500
Paul Schwendenman <schwendenman.paul@gmail.com>
2024-01-21 02:03:41 -0500

    Make refresh tokens renewable

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -150,7 +150,7 @@ REST_FRAMEWORK = {
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
-    'ROTATE_REFRESH_TOKENS': False,
+    'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': False,
     'UPDATE_LAST_LOGIN': True,
     'SIGNING_KEY': 'complexsigningkey',  # generate a key and replace me
