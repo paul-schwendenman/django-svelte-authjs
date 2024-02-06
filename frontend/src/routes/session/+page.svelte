7c301434ec5b6c8d53e856289f7e9729a5899391
Paul Schwendenman <schwendenman.paul@gmail.com>
2024-02-05 20:57:05 -0500

    Add json decoding to the session page

--- frontend/src/routes/session/+page.svelte
+++ frontend/src/routes/session/+page.svelte
@@ -1,5 +1,24 @@
 <script lang="ts">
 	import { page } from '$app/stores';
+
+	const decodeJWT = (token: string) => {
+		const parts = token.split('.');
+
+		if (parts.length !== 3) {
+			throw new Error('Invalid JWT format');
+		}
+
+		const decodedHeader = atob(parts[0]);
+		const decodedPayload = atob(parts[1]);
+
+		const header = JSON.parse(decodedHeader);
+		const payload = JSON.parse(decodedPayload);
+
+		return {
+			header,
+			payload
+		};
+	}
 </script>
 
 <div>
@@ -10,7 +29,17 @@
 		<p>Session reference: {new Date($page.data.session?.ref * 1000).toLocaleString()}</p>
 		<pre style="overflow-y: auto;">
 {JSON.stringify($page.data.session, null, 4)}
-        </pre>
+		</pre>
+		Access token:
+		<p>Access token expiry: {new Date(decodeJWT($page.data.session.access_token)?.payload?.exp * 1000).toLocaleString()}</p>
+		<pre style="overflow-y: auto;">
+{JSON.stringify(decodeJWT($page.data.session.access_token), null, 4)}
+		</pre>
+		Refresh token:
+		<p>Refresh token expiry: {new Date(decodeJWT($page.data.session.refresh_token)?.payload?.exp * 1000).toLocaleString()}</p>
+		<pre style="overflow-y: auto;">
+{JSON.stringify(decodeJWT($page.data.session.refresh_token), null, 4)}
+		</pre>
 	{:else}
 		<h1>Access Denied</h1>
 		<p>
