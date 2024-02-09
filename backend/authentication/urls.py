eb3f822d60dd2c9d2ad647e901fc8ffbe1b6c08f
Paul Schwendenman <schwendenman.paul@gmail.com>
2024-02-08 23:52:36 -0500

    Replace base dj_rest_auth urls

--- backend/authentication/urls.py
+++ backend/authentication/urls.py
@@ -2,17 +2,13 @@
 from dj_rest_auth.jwt_auth import get_refresh_view
 from dj_rest_auth.registration.views import RegisterView
 from dj_rest_auth.views import LoginView, LogoutView, UserDetailsView
-from django.urls import path
+from django.urls import path, include
 from rest_framework_simplejwt.views import TokenBlacklistView, TokenVerifyView
 from authentication.views import GithubLogin, GoogleLogin
 
 urlpatterns = [
+    path('', include('dj_rest_auth.urls')),
     path("register/", RegisterView.as_view(), name="rest_register"),
-    path("login/", LoginView.as_view(), name="rest_login"),
-    path("logout/", LogoutView.as_view(), name="rest_logout"),
-    path("user/", UserDetailsView.as_view(), name="rest_user_details"),
-    path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
-    path("token/refresh/", get_refresh_view().as_view(), name="token_refresh"),
     path('token/blacklist/', TokenBlacklistView.as_view(), name='token_blacklist'),
     path("github/", GithubLogin.as_view(), name="github_login"),
     path("google/", GoogleLogin.as_view(), name="google_login"),
