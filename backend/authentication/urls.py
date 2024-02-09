213772325d043e2395cff1cbdd1b05d7c0a48aac
Paul Schwendenman <schwendenman.paul@gmail.com>
2024-02-08 23:59:10 -0500

    Clean up imports

--- backend/authentication/urls.py
+++ backend/authentication/urls.py
@@ -1,9 +1,6 @@
 
-from dj_rest_auth.jwt_auth import get_refresh_view
-from dj_rest_auth.registration.views import RegisterView
-from dj_rest_auth.views import LoginView, LogoutView, UserDetailsView
 from django.urls import path, include
-from rest_framework_simplejwt.views import TokenBlacklistView, TokenVerifyView
+from rest_framework_simplejwt.views import TokenBlacklistView
 from authentication.views import GithubLogin, GoogleLogin
 
 urlpatterns = [
